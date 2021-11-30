0aab30935ada39cd6e1adaf9ad69bb805639bc74
Alexandr Rebikov <rebikovlk@mail.ru>
2021-11-30 16:26:52 +0700

    cors

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const fs = require('fs');
 const https = require('https');
+const cors = require('@koa/cors');
 const port = process.env.PORT || 3333;
 
 const uploads = path.join(__dirname, '/uploads');
@@ -14,34 +15,34 @@ app.use(koaBody({
     urlencoded:true,
     multipart: true,
 }));
-
+app.use(cors());
 app.use(koaStatic(uploads));
 
 let list = fs.readdirSync(uploads);
 
-// app.use(async (ctx, next) => {
-//     const origin = ctx.request.get('Origin');
-//     if (!origin) {
-//         return await next();
-//     }
-//     const headers = {'Access-Control-Allow-Origin':'*',};
-//     if (ctx.request.method!=='OPTIONS') {
-//         ctx.response.set({...headers});
-//         try {
-//             return await next();
-//         } catch (e) {
-//             e.headers = {...e.headers, ...headers};
-//             throw e;
-//         }
-//     }
-//     if (ctx.request.get('Access-Control-Request-Method')) {
-//         ctx.response.set({...headers,'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE, PATCH',});
-//         if (ctx.request.get('Access-Control-Request-Headers')) {
-//             ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-//         }
-//         ctx.response.status = 204;// No content
-//     }
-// });
+app.use(async (ctx, next) => {
+    const origin = ctx.request.get('Origin');
+    if (!origin) {
+        return await next();
+    }
+    const headers = {'Access-Control-Allow-Origin':'*',};
+    if (ctx.request.method!=='OPTIONS') {
+        ctx.response.set({...headers});
+        try {
+            return await next();
+        } catch (e) {
+            e.headers = {...e.headers, ...headers};
+            throw e;
+        }
+    }
+    if (ctx.request.get('Access-Control-Request-Method')) {
+        ctx.response.set({...headers,'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE, PATCH',});
+        if (ctx.request.get('Access-Control-Request-Headers')) {
+            ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+        }
+        ctx.response.status = 204;// No content
+    }
+});
 
 app.use(async ctx => {
     const method = ctx.request.query.method;
